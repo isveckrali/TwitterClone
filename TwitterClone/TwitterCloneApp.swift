993db6128dc19aa21ee5c04883fa541f10d82995
Mehmet Can Seyhan <mehmet.can.seyhan1@gmail.com>
2021-05-26 16:09:56 +0200

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Mehmet Can Seyhan on 2021-05-26.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
